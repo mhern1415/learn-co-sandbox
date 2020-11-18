ca33c944db65b00424ca456db30bcc9d2373a9ad
Mark Hernandez <m.hern1415@gmail.com>
2020-11-18 01:50:17 +0000

    Automatically backed up by Learn

--- bay_area_events/lib/bay_area_events.rb
+++
@@ -1,6 +0,0 @@
-require "bay_area_events/version"
-
-module BayAreaEvents
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- bay_area_events/lib/bay_area_events/version.rb
+++
@@ -1,3 +0,0 @@
-module BayAreaEvents
-  VERSION = "0.1.0"
-end
